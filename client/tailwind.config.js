3f3a1d0482782f5de9027c1d9e09aece58b605e6
Ayush Kashyap <ayushprokashyap@gmail.com>
2025-03-30 13:48:02 +0530

    custom-css

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      transitionProperty: {
+        transform: "transform",
+      },
+      zIndex: {
+        40: "40",
+        50: "50",
+      },
       fontFamily: {
         ysabeau: ['"Ysabeau SC"', "sans-serif"],
         poppins: ["Poppins", "sans-serif"],
