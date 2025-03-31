eab5994a6db088936323c83954cf6914622d84d3
Ayush Kashyap <ayushprokashyap@gmail.com>
2025-03-31 13:02:46 +0530

    default-theme changed

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -34,14 +34,14 @@ const useUserInfoStore = create<UserInfoState>((set) => ({
   setUserInfo: (userInfo) => set({ userInfo }),
   clearUserInfo: () => set({ userInfo: null }),
   // Initialize theme from localStorage, defaulting to "black"
-  theme: localStorage.getItem("theme") === "white" ? "white" : "black",
+  theme: localStorage.getItem("theme") === "black" ? "black" : "white",
   setTheme: (theme) => {
     localStorage.setItem("theme", theme);
     set({ theme });
   },
   toggleTheme: () =>
     set((state) => {
-      const newTheme = state.theme === "white" ? "black" : "white";
+      const newTheme = state.theme === "black" ? "white" : "black";
       localStorage.setItem("theme", newTheme);
       return { theme: newTheme };
     }),
